0e2973dc47f1f6f562640d8213fc9f6876c3ad34
Taamannae Taabassum <Taamannae@users.noreply.github.com>
2017-11-16 19:39:19 -0500

    Mice click change

--- js/app.js
+++ js/app.js
@@ -1,43 +1,52 @@
 
 $(document).ready(function(){
-    $('#alc1').click(function(){
-     document.getElementById("alc1").style.display = "none";
-     document.getElementById("alc12").style.display = "inline-block";
-    });
 
-    $('#alc2').click(function(){
-     document.getElementById("alc2").style.display = "none";
-     document.getElementById("alc22").style.display = "inline-block";
-      document.getElementById("mice2").style.display = "block";
-      $(".kid-mice").removeClass('active');
-      document.getElementById("mice1").style.display = "none";
-      document.getElementById("mice3").style.display = "none";
-      document.getElementById("mice4").style.display = "none";
+  var counter = 0;
 
 
-    });
+  $('.button').click(function(){
 
-    $('#alc3').click(function(){
-     document.getElementById("alc3").style.display = "none";
-     document.getElementById("alc32").style.display = "inline-block";
-      document.getElementById("mice3").style.display = "block";
-      $(".kid-mice").removeClass('active');
-      document.getElementById("mice1").style.display = "none";
-      document.getElementById("mice2").style.display = "none";
-      document.getElementById("mice4").style.display = "none";
+    if(counter == 0){
+      document.getElementById("alc1").style.display = "none";
+      document.getElementById("alc12").style.display = "inline-block";
 
-    });
+    }else if (counter == 1) {
+      document.getElementById("alc2").style.display = "none";
+      document.getElementById("alc22").style.display = "inline-block";
+       document.getElementById("mice2").style.display = "block";
+       $(".kid-mice").removeClass('active');
+       document.getElementById("mice1").style.display = "none";
+       document.getElementById("mice3").style.display = "none";
+       document.getElementById("mice4").style.display = "none";
 
-    $('#alc4').click(function(){
-     document.getElementById("alc4").style.display = "none";
-     document.getElementById("alc41").style.display = "inline-block";
-      document.getElementById("mice4").style.display = "block";
-      $(".kid-mice").removeClass('active');
-      document.getElementById("mice1").style.display = "none";
-      document.getElementById("mice2").style.display = "none";
-      document.getElementById("mice3").style.display = "none";
+    }else if (counter == 2){
+      document.getElementById("alc3").style.display = "none";
+      document.getElementById("alc32").style.display = "inline-block";
+       document.getElementById("mice3").style.display = "block";
+       $(".kid-mice").removeClass('active');
+       document.getElementById("mice1").style.display = "none";
+       document.getElementById("mice2").style.display = "none";
+       document.getElementById("mice4").style.display = "none";
 
-    });
+    }else if (counter == 3){
+      document.getElementById("alc4").style.display = "none";
+      document.getElementById("alc41").style.display = "inline-block";
+       document.getElementById("mice4").style.display = "block";
+       $(".kid-mice").removeClass('active');
+       document.getElementById("mice1").style.display = "none";
+       document.getElementById("mice2").style.display = "none";
+       document.getElementById("mice3").style.display = "none";
+
+    }else if(counter > 3){
+      console.log(counter);
+
+    }
+    counter = counter + 1;
+
+
+  });
+
+
 
 
 });
